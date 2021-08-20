246beb9d4af70fff00c0d53896625ca88e9aa986
ClarkeFL <fabioliecl@gmail.com>
2021-08-20 18:23:26 +1000

    fixing weird VENV

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.lang.Builder import Builder
 
-Builder.load_file("menu.kv")
 
-class MainWidget(relativelayout):
+class MainWidget():
     pass
 
 class TheLabApp(App):
